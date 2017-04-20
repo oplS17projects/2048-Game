4008f2d02492c83297b67ed3716b37fc8c907f9d
Henry Walker <henrywalker@gmail.com>
2017-04-20 12:48:00 -0400

    Started work on menu gui
    changes screens when your press space-bar (doesn't delete old screen yet unfortunately)

---
+++ menu_gui.rkt
@@ -0,0 +1,17 @@
+#lang racket
+(require 2htdp/universe)
+(require 2htdp/image)
+
+(define (start-screen n)
+  (bitmap "StartScreen.png"))
+
+(define (stop-screen n)
+  (bitmap "StopScreen.png"))
+
+
+(define (key-event-handler x key)
+(if (key=? key " ")      
+      (big-bang 0(to-draw stop-screen))
+    x))
+
+ (big-bang 0 (on-key key-event-handler) (to-draw start-screen))
